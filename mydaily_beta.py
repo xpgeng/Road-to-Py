3f78e1cd2c1c2a0c7ac0cf4bdeb7b4d1304f7c3a
xpgeng <xpgeng@126.com>
2015-10-21 21:41:23 +0800

    modify mydaily.py

--- mydaily_beta.py
+++ mydaily_beta.py
@@ -1,16 +1,17 @@
-
-import sys
+# -*-coding: utf-8 -*-
 from datetime import date
 
-
-choice_command = raw_input('Hey! buudy! write a new of quit,hmm...?> ')
+#get the commands of  quit, write or check the past
+choice_command = raw_input('Hey! buddy!\n q:quit\nw:write a new dairy \n p:check the past diaries?\n> ')
 
 if choice_command == "q": #here maybe can add several words
 	print 'See you later..'
 	quit() 
-elif choice_command == "y":
+#write the diary
+elif choice_command == "w":
 	input_content = raw_input('Let\'s write: ')
 	save_or_not = raw_input('Do you want to save this dairy? y/n: ')
+	
 	if save_or_not == "y":
 	    with open('dairy.md','a') as f:
 	    	now = date.today()
@@ -20,7 +21,8 @@ elif choice_command == "y":
 		print "Your dairy has been deleted..."
 		quit# here can add  write another one, maybe will use some modules
 		    #or defs... cycle?
-elif choice_command == "b":
+#show all the past diaries
+elif choice_command == "p":
     with open('dairy.md','r') as f:
         old_dairy = f.read()
         print old_dairy 
@@ -30,19 +32,5 @@ else:
 f.close()
 
 
-
-
-
-
-
-
-#birth = date(1989, 10, 29)
-#now = date.today()
-
-#now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
-
-#age = now - birth
-
-#print age.days
 
 
