eeaf1cac66352fc29a73743bacf0c684baff5a06
xpgeng <xpgeng@126.com>
2016-01-10 16:28:33 +0800

    add main.py about class

--- 16y3w/main.py
+++ 16y3w/main.py
@@ -8,27 +8,36 @@ self is a alias of the real object
 
 class Book:
     x = 0
+    name = ""
     
-    def __init__(self):
-        print "I'm a book."
+    def __init__(self,name): # common
+        self.name = name
 
     def readtimes(self): 
         self.x = self.x + 1
-        print "This book was read %s times." %self.x 
+        print "%s was read %s times." %(self.name, self.x)
+    
+    #def __del__(self): # seldom used
+    #    print "I'm deleted...", self.x
+
+a = Book("complexity")
+a.name
+a.readtimes()
+
+b = Book("OutofControl")
+b.name
+b.readtimes()
 
 
-complexity = Book()
-complexity.readtimes()
-complexity.readtimes()
-complexity.readtimes()
-print complexity.x
-print dir(complexity) # show the operations that the object can perform
 
 
-y = "hello world"
+#print dir(complexity) # show the operations that the object can perform
+
+
+#y = "hello world"
 
 #print dir(y)
-print y.split()
+#print y.split()
 
 
 
